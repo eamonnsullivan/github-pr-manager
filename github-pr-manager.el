436382ccb918f11a9ae326270b0c92d25114b92d
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2020-10-25 19:55:26 +0000

    Add-comment

--- github-pr-manager.el
+++ github-pr-manager.el
@@ -6,6 +6,12 @@
                :namespace eamonnsullivan.github-pr-manager
                :doc "Get information on a Github pull request.")
 
+(clomacs-defun gprm-add-pull-request-comment
+               add-comment
+               :lib-name "github-pr-manager"
+               :namespace eamonnsullivan.github-pr-manager
+               :doc "Add a comment to a github pull request.")
+
 (defun get-hashmap (response)
   "Get a hashmap from a JSON response"
   (when response
@@ -16,10 +22,17 @@
       hashmap)))
 
 (defun gprm-get-info (url)
+  "Get information, the form of a hashmap, for a particular pull request."
   (interactive "r")
   (let ((response (gprm-pull-request-info url))
         (nrepl-sync-request-timeout 30))
     (get-hashmap response)))
 
+(defun gprm-add-comment (url comment)
+  "Add a comment to a pull request."
+  (interactive "p")
+  (let ((response (gprm-pull-request-comment url comment))
+        (nrepl-sync-request-timeout 30))
+    (get-hashmap response)))
 
 (provide 'github-pr-manager)
